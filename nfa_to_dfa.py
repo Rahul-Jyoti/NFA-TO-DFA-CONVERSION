d6a1fb78d9d5a5ed6289f01182ca485351d40891
Rahul Jyoti <40001136+Rahul-Jyoti@users.noreply.github.com>
2019-09-03 20:30:05 +0530

    added example output

--- nfa_to_dfa.py
+++ nfa_to_dfa.py
@@ -63,3 +63,67 @@ print(dfa)                                           #Printing the DFA created
 print("\nPrinting DFA table :- ")
 dfa_table = pd.DataFrame(dfa)
 print(dfa_table.transpose())
+
+ 
+
+"""
+# Code Ouput Example
+
+No. of states : 4
+No. of transitions : 2
+state name : A
+path : a
+Enter end state from state A travelling through path a : 
+A B
+path : b
+Enter end state from state A travelling through path b : 
+A
+state name : B
+path : a
+Enter end state from state B travelling through path a : 
+C
+path : b
+Enter end state from state B travelling through path b : 
+C
+state name : C
+path : a
+Enter end state from state C travelling through path a : 
+D
+path : b
+Enter end state from state C travelling through path b : 
+D
+state name : D
+path : a
+Enter end state from state D travelling through path a : 
+
+path : b
+Enter end state from state D travelling through path b : 
+
+
+NFA :- 
+
+{'A': {'a': ['A', 'B'], 'b': ['A']}, 'B': {'a': ['C'], 'b': ['C']}, 'C': {'a': ['D'], 'b': ['D']}, 'D': {'a': [], 'b': []}}
+
+Printing NFA table :- 
+        a    b
+A  [A, B]  [A]
+B     [C]  [C]
+C     [D]  [D]
+D      []   []
+
+DFA :- 
+
+{'A': {'a': 'AB', 'b': 'A'}, 'AB': {'a': 'ABC', 'b': 'AC'}, 'ABC': {'a': 'ABCD', 'b': 'ACD'}, 'AC': {'a': 'ABD', 'b': 'AD'}, 'ABCD': {'a': 'ABCD', 'b': 'ACD'}, 'ACD': {'a': 'ABD', 'b': 'AD'}, 'ABD': {'a': 'ABC', 'b': 'AC'}, 'AD': {'a': 'AB', 'b': 'A'}}
+
+Printing DFA table :- 
+         a    b
+A       AB    A
+AB     ABC   AC
+ABC   ABCD  ACD
+AC     ABD   AD
+ABCD  ABCD  ACD
+ACD    ABD   AD
+ABD    ABC   AC
+AD      AB    A
+
+"""
